947b3532113083a665ed0b7cd229fac08bb0c90d
Priyanshu Kumar <priyanshu3960@gmail.com>
2026-02-01 23:12:57 +0530

    integrating backend to frontend

--- Day-03/src/app.js
+++ Day-03/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const noteModel = require("./modals/note.modal")
+const cors = require("cors")
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.post("/notes", async (req, res)=> {
@@ -24,6 +26,24 @@ app.get("/notes", async (req, res)=> {
     res.status(200).json({
         message: "Note Fetched Successfully",
         notes
+    })
+})
+
+app.delete("/notes/:id", async (req, res)=> {
+    const index = req.params.id;
+    await noteModel.findByIdAndDelete(index)
+    res.status(200).json({
+        message: "Note Deleted Successfully"
+    })
+})
+
+app.patch("/notes/:id", async (req, res)=> {
+    const id = req.params.id
+    const {description} = req.body
+
+    await noteModel.findByIdAndUpdate(id, {description})
+    res.status(200).json({
+         message: "Note Updated Successfully"
     })
 })
 
